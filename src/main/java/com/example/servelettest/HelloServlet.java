65db57cdc1c6c4eca67fb8079e7f577aa9ce5335
quyixiao <2621048238@qq.com>
2022-07-20 18:35:49 +0800

    rjquwhnt

--- src/main/java/com/example/servelettest/HelloServlet.java
+++ src/main/java/com/example/servelettest/HelloServlet.java
@@ -1,13 +1,11 @@
 package com.example.servelettest;
 
 
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 
 public class HelloServlet extends HttpServlet {
@@ -15,18 +13,18 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req,resp);
+        super.doGet(req, resp);
         System.out.println("doGet方法执行");
 
-        int i = 0 ;
+        int i = 0;
         int j = 0;
-        int c = i / j ;
+        int c = i / j;
         resp.getWriter().append("xxxxxx");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet方法执行");
-        doGet(req,resp);
+        doGet(req, resp);
     }
 }
