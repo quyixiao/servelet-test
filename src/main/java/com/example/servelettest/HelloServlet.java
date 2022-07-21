820e5e0288fef770614cd9a52a9d08094caae43a
quyixiao <2621048238@qq.com>
2022-07-21 12:37:38 +0800

    rjuqwhnt

--- src/main/java/com/example/servelettest/HelloServlet.java
+++ src/main/java/com/example/servelettest/HelloServlet.java
@@ -1,13 +1,16 @@
 package com.example.servelettest;
 
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Field;
 
-
+@WebServlet( asyncSupported = true)
 public class HelloServlet extends HttpServlet {
 
 
@@ -15,10 +18,36 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
         System.out.println("doGet方法执行");
+        // 释放http连接，转为异步
+
+      /*  String name = req.getClass().getName();
+        System.out.println("servlet name is :" + name);
+        try {
+            Field field = req.getClass().getDeclaredField("request");
+            field.setAccessible(true);
+            Object ob = field.get(req);
+            System.out.println("request name is :" + ob.getClass().getName());
+
+
+            Field async = ob.getClass().getDeclaredField("asyncSupported");
+            async.setAccessible(true);
+            async.set(ob,true);
+            Object asyncSupported = async.get(ob);
+            System.out.println("========asyncSupported====="+asyncSupported);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+
+        AsyncContext context = req.startAsync();
+
+        // 4秒才超时了，超时也会中断当前请求直接返回
+        context.setTimeout(4000L);
 
         int i = 0;
         int j = 0;
         int c = i / j;
+
         resp.getWriter().append("xxxxxx");
     }
 
@@ -26,5 +55,8 @@ public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet方法执行");
         doGet(req, resp);
+    }
+
+    private class RequestFacade {
     }
 }
